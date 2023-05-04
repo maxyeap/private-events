f5e01382050b7308d78e651a078ef167954ccedd
Max Yeap <max@yongtuck.com>
2023-05-04 15:28:19 +0800

    Add fk constraint to creator_id to Event model

---
+++ db/migrate/20230503165201_add_fk_to_events.rb
@@ -0,0 +1,5 @@
+class AddFkToEvents < ActiveRecord::Migration[7.0]
+  def change
+    add_column(:events, :creator_id, :integer, null: false)
+  end
+end
---
+++ db/migrate/20230504072459_add_fk_constraint_to_creator_id.rb
@@ -0,0 +1,5 @@
+class AddFkConstraintToCreatorId < ActiveRecord::Migration[7.0]
+  def change
+    add_foreign_key :events, :users, column: :creator_id
+  end
+end
